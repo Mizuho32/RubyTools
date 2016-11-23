96e4bf7484ca27479714d0cea4363932f8cc3cbe
Mizuho <dummy32@hotmail.co.jp>
2016-11-23 15:28:55 +0900

    install script modified

--- asciibuild/lib/install.rb
+++ asciibuild/lib/install.rb
@@ -2,5 +2,12 @@ module Install
   TargetDir = Pathname(ARGV[1] || "~/bin").expand_path.to_s
 
   Dir::mkdir("#{TargetDir}/") unless File.exists?("#{TargetDir}/")
-  FileUtils.ln_s("#{Main::FILE_DIR}/#{Main::FILE}", "#{TargetDir}/")
+  File.open("#{TargetDir}/#{Main::FILE}", "w"){|f|
+    f.write <<-"EOF"
+#!/bin/sh
+
+BUNDLE_GEMFILE=#{Main::FILE_DIR}/Gemfile #{Main::FILE_DIR}/asciibuild "$@"
+EOF
+  FileUtils.chmod("u+x", "#{TargetDir}/#{Main::FILE}")
+  }
 end
