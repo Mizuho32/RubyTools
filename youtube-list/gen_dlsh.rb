be9a8a114edcbd707eb7ff12b74ad8b4fac502f4
Mizuho <mizuho128@gmail.com>
2025-06-14 23:32:12 +0900

    gen_dlsh.rb remotestdio able code

--- youtube-list/gen_dlsh.rb
+++ youtube-list/gen_dlsh.rb
@@ -3,11 +3,18 @@
 
 require 'pathname'
 require 'yaml'
+require 'open3'
+
+require_relative 'lib'
+
+begin
 
 # ARGV: list.yaml [last_idx]
 
+
 yaml_path = Pathname(ARGV.first)
 target_dir = yaml_path.dirname
+puts "Download #{target_dir}"
 
 downloadeds = target_dir
   .glob("*")
@@ -35,14 +42,15 @@ d_in_y = downloadeds.group_by{|idx, id, name|
 }
 
 if d_in_y[false]&.empty? == true then
-  puts 'Add deleteds to yaml...'
-  d_in_y[false].each{|idx,id,name|
+  deleteds = d_in_y[false].map{|idx,id,name|
     name = name.sub(/^\d+ - /, '').sub(/ #{id}/, '').strip
     url = "https://www.youtube.com/watch?v=#{id}"
-    puts "  add #{idx} #{name}"
     yaml.insert(idx - 1, {name: name, url: url})
-  }
-  print('Update yaml?>>')
+    "  add #{idx} #{name}"
+  }.join("\n")
+  print """Add deleteds to yaml...
+#{deleteds}
+Update yaml?>>"""
   if not gets =~ /no?/i then
     File.write(yaml_path, yaml.to_yaml)
   end
@@ -52,11 +60,12 @@ if not downloadeds.map{|i, id, name| yaml_map.has_key? id }.all? then
   $stderr.puts "Inconsistent! Exit"
   exit 1
 else
-  puts "# Consistency OK"
+  puts "Consistency OK"
 end
 
-
 # Gen download script
+#Dir::chdir(target_dir)
+
 y_in_d = yaml_map.keys.each_with_index.group_by{|id, idx|
   downloadeds_map.has_key? id
 }
@@ -65,20 +74,49 @@ newly_downloads = (y_in_d[false]||[])
   .select{|id, idx| yaml_map[id][:name] !~ /^(delete|private)/i }
   .sort{|(_, idxl),(_,idxr)| idxl <=> idxr }
 
+if newly_downloads.empty? then
+  puts "No updates. End."
+  exit
+end
+
 last_idx = (ARGV[1]|| -1).to_i
 last_index = downloadeds[last_idx].first
-puts "# last_index #{last_index} #{last_idx}"
+puts "Last_index #{last_index} #{last_idx}"
 
-cmds = newly_downloads.each_with_index.map{|(id, _), i|
+result = newly_downloads.each_with_index.map{|(id, _), i|
+  next if yaml_map[id][:skip]
+
   idx = last_index + i
-  out_path = (target_dir / "#{idx.succ} - %(title)s %(id)s.%(ext)s").to_s
   url = yaml_map[id][:url]
-  cmd = %Q|yt-dlp --match-filter "duration < 1200" -o "#{out_path}" "#{url}"|
+  durations = yaml_map[id][:durations] || ['']
+  out = durations.map {|duration|
+    durat_txt = if duration.empty? then duration else "_#{duration}" end
+    durat_opt = if duration.empty? then ' --match-filter "duration < 1200"' else %Q|--download-sections "*#{duration}"| end
+    
+    out_path = (target_dir / "#{idx.succ} - %(title)s#{durat_txt} %(id)s.%(ext)s").to_s
+    cmd = %Q|yt-dlp -o "#{out_path}" #{durat_opt} "#{url}"|
+
+    name = yaml_map[id][:name]
+    out, err, status = Open3.capture3(cmd)
+    if status.exitstatus.zero? then
+      name << " OK"
+    else
+      name << " Err\n#{err}"
+    end
+    name << "\n#{cmd}"
+    name
+  }.join("\n")
+  puts out
 }.join("\n")
 
-puts cmds
+#puts result
 
 #puts "last number is #{last}"
 #puts <<-"EOF"
 #yt-dlp --match-filter "duration < 1200" --playlist-start #{last+1} -o "%(playlist_index)s - %(title)s %(id)s.%(ext)s" "https://www.youtube.com/playlist?list=PLavJaWSqKmCx3p3GTL2rTrkrls6aptcX1"
 #EOF
+
+rescue StandardError => ex
+  puts("Error: #{__FILE__} #{ARGV.join(' ')}}", ex.message, ex.backtrace.join("\n"))
+  exit 1
+end
