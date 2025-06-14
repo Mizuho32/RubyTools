a921eed38a22c613dafef8c6704800772b8e8593
Mizuho <mizuho128@gmail.com>
2025-06-14 23:31:52 +0900

    main.rb remotestdio able code

--- youtube-list/main.rb
+++ youtube-list/main.rb
@@ -8,6 +8,7 @@ require 'google/apis/youtube_v3'
 
 require_relative "lib"
 
+begin
 # ARGV: list.yaml  apikey.txt  listid.txt [last_idx]
 
 listyaml = Pathname(ARGV.first)
@@ -24,11 +25,51 @@ videos = get_until(tube, ARGV[2].to_s, last_name, max_results: 10)
     {name: itm[:title], url: "https://www.youtube.com/watch?v=#{ itm[:video_id] }" }
   }.reverse
 
+if videos.empty? then
+  puts "No update for #{listyaml}. End."
+  exit
+end
 
-puts "Delta: \n#{videos.map{|itm| itm[:name]}.join("\n")}\n------"
-print "OK?>>"
-STDIN.gets
+print """
+Delta:
+#{videos.map{|itm| itm[:name]}.join("\n")}
+------
+OK?>>"""
+ret = safe_gets()
+
+if !ret.downcase.include?(?y) then
+  puts 'End'
+  exit
+end
 
 yml = (list + videos).to_yaml
 #puts yml
 File.write(listyaml, yml)
+
+## git commit
+Dir::chdir(listyaml.parent)
+
+listyamlname = listyaml.basename.to_s
+videonames = videos.map{|itm| itm[:name]}
+diff = `git diff #{listyamlname}`
+cmd = "git add #{listyamlname} && git commit -m '#{listyamlname} #{videonames.first} - #{videonames[-1]}'"
+print """
+Diff is:
+#{diff}
+---
+will run:
+$ #{cmd}
+Diff OK? >>"""
+
+if safe_gets().downcase.include?(?y) then
+  ret =  system(cmd)
+  puts "git commit with return #{ret}"
+else
+  puts 'No git commit'
+  exit
+end
+
+rescue StandardError => ex
+  puts("Error: #{__FILE__} #{ARGV.join(' ')}}", ex.message, ex.backtrace.join("\n"))
+  exit 1
+end
