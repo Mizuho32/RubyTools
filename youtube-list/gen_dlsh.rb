55df65f094c55fb5246cdfc7d6372411d1e015e7
Mizuho <mizuho128@gmail.com>
2025-07-05 10:35:36 +0900

    gen_dlsh.rb index consistency check

--- youtube-list/gen_dlsh.rb
+++ youtube-list/gen_dlsh.rb
@@ -4,7 +4,7 @@
 require 'pathname'
 require 'yaml'
 require 'open3'
-
+require 'fileutils'
 require_relative 'lib'
 
 begin
@@ -24,6 +24,7 @@ downloadeds = target_dir
     /^(\d+)/.match(lname)[1].to_i(10) <=> /^(\d+)/.match(rname)[1].to_i(10)
   }.map{|name|
     [/^(\d+)/.match(name)[1].to_i(10), /([^\s]+)\.[^\.]+$/.match(name)[1].to_sym, name] } # video id
+# idx, id, filename
 
 #puts downloadeds
 
@@ -31,7 +32,7 @@ downloadeds_map = Hash[downloadeds.map{|idx, id, name| [id, [idx, name]]}]
 
 
 yaml = YAML.load_file(yaml_path)
-yaml_map = Hash[ yaml.map{|item| [item[:url][/=([^=]+)/, 1].to_sym, item]} ]
+yaml_map = Hash[ yaml.map{|item| [item[:url][/=([^=]+)/, 1].to_sym, item]} ] # id -> item
 
 # check consistency
 ## check downloadeds in yaml
@@ -59,9 +60,45 @@ end
 if not downloadeds.map{|i, id, name| yaml_map.has_key? id }.all? then
   $stderr.puts "Inconsistent! Exit"
   exit 1
-else
-  puts "Consistency OK"
 end
+
+## idx consistency
+idx_updates = yaml_map
+  .each_with_index.map{|(id, item), idx_yml|
+    idx_d, name_d = downloadeds_map[id]
+    #puts("None for #{id}") if name_d.nil? || name_d.empty?
+    [idx_yml+1, idx_d, name_d]
+  }.select{|idx_yml, idx_d, name_d|
+    !name_d.nil? && idx_yml != idx_d
+  }
+
+if !idx_updates.empty? then
+  tmp = idx_updates.map{|idx_yml, idx_d, name_d|
+    "#{idx_d}->#{idx_yml} #{name_d}"
+  }.join("\n")
+  print """### index updates
+```
+#{tmp}
+```
+Update indices? >>"""
+
+  if safe_gets.downcase.include?(?y) then
+    idx_updates.each{|idx_yml, idx_d, name_d|
+      newname = name_d.sub(/^[^\s]+/, idx_yml.to_s)
+      src = target_dir / name_d
+      dst = target_dir / newname
+
+      if dst.exist? then
+        $stderr.puts("#{dst} exists! SKip.")
+      else
+        #puts("mv #{src} -> #{dst}")
+        FileUtils.mv(src, dst)
+      end
+    }
+  end
+end
+
+puts "Consistency OK"
 
 # Gen download script
 #Dir::chdir(target_dir)
