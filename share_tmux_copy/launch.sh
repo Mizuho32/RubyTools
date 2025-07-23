7117a445ac786c40a4090769d5011070e34df930
Mizuho <mizuho128@gmail.com>
2025-07-24 00:03:33 +0900

    launch.sh mise init

--- share_tmux_copy/launch.sh
+++ share_tmux_copy/launch.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+export MISE_DATA_DIR=$HOME/media/data/mise
+eval $(mise activate --shims bash)
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+pwd
+
 bundle exec ruby server.rb 8001
