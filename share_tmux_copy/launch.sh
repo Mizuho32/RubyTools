1535828caafb7bc12ef763e848ed577e7e720474
Mizuho <mizuho128@gmail.com>
2025-12-01 13:48:43 +0900

    launch.sh debug prints

--- share_tmux_copy/launch.sh
+++ share_tmux_copy/launch.sh
@@ -4,6 +4,11 @@ export MISE_DATA_DIR=$HOME/media/data/mise
 eval $(mise activate --shims bash)
 
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
-pwd
+
+echo Current is $(pwd), PATH is ${PATH}
+echo ls is $(ls -a)
+echo use bundle: $(which bundle) $(bundle -v)
+echo use ruby: $(bundle exec ruby -v) at $(bundle exec which ruby)
+echo Gems: $(bundle exec gem list)
 
 bundle exec ruby server.rb 8001
