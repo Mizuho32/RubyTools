47b41e14df553c263c54c640530a79e5f12463b5
Mizuho <mizuho128@gmail.com>
2026-03-26 22:09:16 +0900

    server.rb wayland clipboard

--- share_tmux_copy/server.rb
+++ share_tmux_copy/server.rb
@@ -3,8 +3,16 @@
 
 require 'json'
 require 'sinatra'
+require 'pathname'
 
 ENV['DISPLAY'] = ':0' unless ENV['DISPLAY']
+copy_cmd = if system("which xsel") then
+            "xsel -i --clipboard"
+           elsif system("which wl-copy") then
+             "wl-copy"
+           else
+             exit 2
+           end
 
 post '/' do
   # リクエストボディを取得して標準出力に出力
@@ -14,13 +22,11 @@ post '/' do
 
   if input_data then
     # copy to clipboard
-    command = "xsel -i --clipboard"
-
-    IO.popen(command, "r+") do |io|
+    IO.popen(copy_cmd, "r+") do |io|
       io.print input_data
       io.close_write
 
-      puts "xsel: #{io.read}"
+      puts "to clipbard: #{io.read}"
     end
 
     # レスポンスを返す
